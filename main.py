0977a770ece0881a763c7cdd1ecad45e5a380765
Phan Thanh Giảng <giangb1609816@student.ctu.edu.vn>
2022-05-20 10:41:19 +0700

    Cập nhật để hiển thị dạng web HTML

--- main.py
+++ main.py
@@ -1,52 +1,38 @@
 import requests
 import json
 import os
-import urllib3
-import sys
-import wget
 
-image_url = 'https://cf.shopee.vn/file/'
-offset = 1
-count = 0
+def _get_image_from_shopee_url(url):
+    try:
+        shop_id = url.split('.')[2]
+        item_id = url.split('.')[3].split('?')[0]
+        url = 'https://shopee.vn/api/v2/item/get_ratings?filter=3&flag=1&itemid='+item_id+'&shopid='+shop_id+'&type=0'
+        # list_of_images = []
+        data = json.loads(requests.get(url).text)
+        ratings = data['data']['ratings']
+        list_of_media = []
+        for i in ratings:
+            list_of_images = [x for x in i['images']]
+            list_of_videos = [x['url'] for x in i['videos']]
+            list_of_media.append(list_of_images + list_of_videos)
+        return list_of_media
+    except:
+        print('Error')
 
-input_url = input('Enter url: ')
+def _create_html_file(list_of_images):
+    html = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8" /><meta name="viewport" content="width=device-width, initial-scale=1" /><title>Bootstrap demo</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"  crossorigin="anonymous"/></head><body><div class="container"><div class="row mt-3">'
+    for image in list_of_images:
+        for img in image:
+            if img.split('.')[-1] == 'mp4':
+                html += '<div class = "col-sm-3" ><div class = "card mt-3" ><iframe class="embed-responsive-item" src="'+img+'" allowfullscreen></iframe></div ></div >'
+            else:
+                html += '<div class = "col-sm-3" ><div class = "card mt-3" ><img src = "https://cf.shopee.vn/file/'+img+'" class = "card-img-top" ></div ></div >'
+    html += '</div ></div ><script  src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity = "sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin = "anonymous"> < /script ></body></html>'
 
-# Get shopid, itemid
-try:
-    ids = input_url.split('/')[-1]
-    shopid = ids.split('.')[1]
-    itemid = ids.split('.')[2].split('?')[0]
-    # Get number of reviews with image
-    limit = 500
-    api_url = 'https://shopee.vn/api/v2/item/get_ratings?filter=3&flag=1&itemid=' + str(itemid) + '&limit=' + str(limit) + '&offset=1&shopid=' + str(shopid) + '&type=0'
-    res = requests.get(api_url)
-    data = json.loads(res.text)
-    numOfImages = int(data['data']['item_rating_summary']['rcount_with_image'])
-    # Create folder
-    folder_name = 'shopee_' + str(shopid) + '_' + str(itemid)
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
-    # Get reviews
-    limit = 50
-    for i in range(0, numOfImages, limit):
-        api_url = 'https://shopee.vn/api/v2/item/get_ratings?filter=3&flag=1&itemid=' + str(itemid) + '&limit=' + str(limit) + '&offset=' + str(offset) + '&shopid=' + str(shopid) + '&type=0'
-        res = requests.get(api_url)
-        data = json.loads(res.text)
-        orders = data['data']['ratings']
-        for order in orders:
-            for image in order['images']:
-                image_url_req = image_url + image
-                image_name = str(image) + '.jpg'
-                # print(image_url_req, end='\r')
-                try:
-                    image_data = requests.get(image_url, stream=True)
-                    # with open(folder_name + '/' + image_name, 'wb') as handler:
-                    wget.download(image_url_req, out=folder_name + '/' + image_name)
-                except:
-                    print('Error: ' + image_url)
-                print('\tDownloaded: ' + image_name + '\t' + str(count) + '/' + str(numOfImages), end='\r')
-                count += 1
-        offset += limit
-except:
-    print('Cannot get shopid or itemid from url')
-    sys.exit()
+    with open('index.html', 'w') as f:
+        f.write(html)
+    print('Done')
+
+if __name__=='__main__':
+    url = input('Enter the url of the product: ')
+    _create_html_file(_get_image_from_shopee_url(url))
